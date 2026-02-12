d30540e07340ae0a9479d4c18a27b243b8dd3361
Muhammad Usama <muhammadusama3228@gmail.com>
2026-02-12 23:22:13 +0500

    add docker docker compose and devops practices

--- django_server/settings.py
+++ django_server/settings.py
@@ -99,9 +99,13 @@ WSGI_APPLICATION = "django_server.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / env('DB_NAME', default='db.sqlite3'),
+    'default': {
+        'ENGINE': f'django.db.backends.{env('DB_ENGINE')}',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST', default='localhost'),
+        'PORT': env('DB_PORT', default=5432),
     }
 }
 
